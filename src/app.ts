d49a4e9ea2d813aae975573c0ef0c8196a680768
Lance Zhou <jiahuasir@qq.com>
2023-08-31 18:49:44 +0800

    message

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,13 @@
 /// <reference path="./basic/a.ts">
-console.log(ABC.VERSION);
+// console.log(ABC.VERSION);
+interface Style {
+    color: string;
+}
+
+interface Shape {
+    name: string;
+}
+
+interface Circle extends Style, Shape {
+    radius: number;
+}
