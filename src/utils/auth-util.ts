9a4907dd188a43e0575d62479b4d7fecb0582ce7
Lance Zhou <jiahuasir@qq.com>
2023-09-01 10:01:39 +0800

    message

--- src/utils/auth-util.ts
+++ src/utils/auth-util.ts
@@ -5,3 +5,16 @@ export function printId(id: number | string) {
         console.log(id);
     }
 }
+
+export function getPort(scheme: 'http' | 'https') {
+    switch (scheme) {
+        case 'http':
+            return 80;
+        case 'https':
+            return 443;
+    }
+}
+
+export function fail():Error {
+    return new Error("Something failed");
+}
